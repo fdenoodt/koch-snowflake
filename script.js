3a14e92b67e98cf379594df8c9ab73face3847ae
Fabian Denoodt <32387007+oBoii@users.noreply.github.com>
2019-01-02 21:11:25 +0100

    Koch Version 1 + other nice looking things

--- script.js
+++ script.js
@@ -4,37 +4,64 @@ let ctx;
 const init = () => {
   c = document.getElementById("myCanvas");
   ctx = c.getContext("2d");
-
-  koch(30, 0, 100, 90)
+  
+  koch(30, 0, 40, 0, -60)
+  koch(30, 100, 50, 0, -7)
+  koch(30, 200, 50, 0, -45)
+  koch(30, 300, 75, 0, 45)
+  koch(30, 400, 100, 0, 90)
 }
 
-const koch = (l, x, y, dir) => {
-  const rad = dir * (Math.PI / 180);
-  const sin = Math.sin(rad);
-  const cos = Math.cos(rad);
+const koch = (l, x, y, deg, offset = 0) => {
+  const l1 = {
+    x1: x,
+    y1: y,
+    x2: x + l * Math.cos(rad(deg)),
+    y2: y + l * Math.sin(rad(deg))
+  }
 
-  const opposite = (sin * l); 
-  const rechteZijde = (cos * l);
-  drawLine(x, y, opposite, (rechteZijde));
+  const l2 = {
+    x1: l1.x2,
+    y1: l1.y2,
+    x2: l1.x2 + l * Math.cos(rad(deg - 60)),
+    y2: l1.y2 + l * Math.sin(rad(deg - 60))
+  }
 
+  const l3 = {
+    x1: l2.x2,
+    y1: l2.y2,
+    x2: l2.x2 + l * Math.cos(rad(deg + 60)),
+    y2: l2.y2 + l * Math.sin(rad(deg + 60))
+  }
 
-  // mijnLijn.X2 = 150 + opposite;
-  // mijnLijn.Y2 = 300 - (rechteZijde + 150);
+  const l4 = {
+    x1: l3.x2,
+    y1: l3.y2,
+    x2: l3.x2 + l * Math.cos(rad(deg)),
+    y2: l3.y2 + l * Math.sin(rad(deg))
+  }
 
+  drawLine(l1);
+  drawLine(l2);
+  drawLine(l3);
+  drawLine(l4);
 
-  // const x1 = x;
-  // const y1 = y;
-  // const x2 =
-  //   drawLine(x, y, x + l, y);
-
-  // drawLine(x + l, y, x + (l) + l * 0.5, y - l);
-  // drawLine(x + l, y, x + (l) + l * 0.5, y - l);
-
-  // drawLine(x + (2 * l), y, x + (3 * l), y);
+  setTimeout(() => {
+    koch(l, l4.x2, l4.y2, deg + 180 + offset, offset)
+  }, 10);
 
 }
 
-const drawLine = (x1, y1, x2, y2) => {
+const rad = (deg) => {
+  return deg * (Math.PI / 180);
+}
+
+const drawLine = (line) => {
+  x1 = line.x1
+  y1 = line.y1
+  x2 = line.x2
+  y2 = line.y2
+
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
